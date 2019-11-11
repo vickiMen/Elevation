b09fcaced4a8c792c0af4af9c7ff86d295ab73f2
Vicki Menashe <vicki.menashe@gmail.com>
2019-11-11 16:30:27 +0200

    Update reservations.js

--- reservations.js
+++ reservations.js
@@ -1,11 +1,13 @@
 const reservations = {
     Bob: { claimed: false },
-    Ted: { claimed: true }
+    Ted: { claimed: true },
+    noName: { claimed: true}
 }
   
 const name = prompt('Please enter the name for your reservation');
+let nameLowerCase = name.toLowerCase()
 
-if (name == "Bob" ) {
+if (nameLowerCase == 'bob') {
     if (reservations.Bob.claimed == false) {
         alert('Hi there Bob! Please have a sit')
     }
@@ -13,12 +15,12 @@ if (name == "Bob" ) {
         alert('We\'re sorry, the reservation is already claimed')
     }
 }
-else if (name == "Ted") {
+else if (nameLowerCase == 'ted') {
     if (reservations.Ted.claimed == false) {
             alert('Hi there Ted!')
     }
     else {
-        alert('We\'re sorry, the reservation is already claimed')
+        alert('We\'re sorry, Ted, the reservation is already claimed')
     }
 }
 else {
